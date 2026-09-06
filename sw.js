05b0a9a2ed2d693ad8c6a4e4047560d15237ce33
mobygun <113428603+mobygun@users.noreply.github.com>
2026-09-06 20:42:38 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gyeongbi-v5";
+const CACHE = "gyeongbi-v6";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./template.js", "./xlsxbuild.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
